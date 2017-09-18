5d153c3dd73bcf912d7c5c9a3f24f1f32ca1e2a2
jenniferneale <jeneale6@gmail.com>
2017-09-18 18:34:32 -0400

    Added a news get

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.get('/',function(req,res){
 	res.send("Why hello there!");
 });
 
+app.get('/news',function(req,res){
+	res.send("Sunny with a chance of meatballs?");
+});
 
 app.listen(3000,function(request,response){
 	console.log("I'm listening!");
