7185793f7300aaff343dae2c5efdedd7b792f9cf
jenniferneale <jeneale6@gmail.com>
2017-09-18 18:32:26 -0400

    Got a listener and a polite response

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const express = require( 'express' );
+const app = express();
+
+app.get('/',function(req,res){
+	res.send("Why hello there!");
+});
+
+
+app.listen(3000,function(request,response){
+	console.log("I'm listening!");
+});
+
 
